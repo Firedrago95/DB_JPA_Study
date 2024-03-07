9bc3c274fffabd3072084590c4ead863d6c7c621
YONGHWA\ekore <yonghwa95@naver.com>
2024-03-07 15:02:55 +0900

    MyBatis 적용법

--- src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -14,7 +14,8 @@ public interface ItemMapper {
 
     void save(Item item);
 
-    void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam);
+    void update(@Param("id") Long id,
+                @Param("updateParam") ItemUpdateDto updateParam);
 
     Optional<Item> findById(Long id);
 
