87b5e9d296d764848e3d2e05072e3df6de5f7c8e
YONGHWA\ekore <yonghwa95@naver.com>
2024-03-07 10:38:30 +0900

    MyBatis 기본 사용법

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,9 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV1Config;
-import hello.itemservice.config.JdbcTemplateV2Config;
-import hello.itemservice.config.JdbcTemplateV3Config;
-import hello.itemservice.config.MemoryConfig;
+import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +16,8 @@ import javax.sql.DataSource;
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(JdbcTemplateV3Config.class)
+//@Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
