628ceae97872c195cd2b6764b8a614a7342c28c5
YONGHWA\ekore <yonghwa95@naver.com>
2024-03-25 10:34:10 +0900

    JPA 사용해보기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,12 +5,30 @@ import jakarta.persistence.*;
 public class JpaMain {
 
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityManager em = emf.createEntityManager();
+
+        // 트랜잭션 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //code
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+
+            // 트랜잭션 커밋
+            tx.commit();
+        } catch (Exception e) {
+            // 문제 있으면 롤백
+            tx.rollback();
+        } finally {
+            // EntityManager 닫기 , 작업마다 생성
+            em.close();
+        }
+        // EntityManagerFactory 하나만 유지
         emf.close();
+
     }
 }
