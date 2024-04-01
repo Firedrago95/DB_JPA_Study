d4a622921f583a9df9a18ae3cd0d73af78254105
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-01 11:03:48 +0900

    고아객체 삭제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -29,6 +29,12 @@ public class JpaMain {
 
             em.persist(parent);
 
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
+
             // 트랜잭션 커밋
             tx.commit();
         } catch (Exception e) {
