184adfcfb46eb74c1538330caf2b42da27739e9c
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-03 10:31:50 +0900

    결과조회 메서드

--- src/main/java/hello/jpa/jpql/JpaMain.java
+++ src/main/java/hello/jpa/jpql/JpaMain.java
@@ -2,6 +2,8 @@ package hello.jpa.jpql;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -17,11 +19,17 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            // 반환 타입이 명확할 때 (TypeQuery< >)
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            // 반환 타입이 명확하지 않을 때 (Query)
-            Query query = em.createQuery("select m.username, m.age from Member m");
+            // getResultList() : 결과없으면 빈 리스트 반환 (null 걱정 x)
+            List<Member> resultList = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+
+            // getSingleResult( ) -> 반환값 하나일때 , 결과없거나, 둘 이상이면 예외발생
+            Member singleResult = em.createQuery("select m from Member m where m.id = 1", Member.class)
+                    .getSingleResult();
 
             tx.commit();
         } catch (Exception e) {
