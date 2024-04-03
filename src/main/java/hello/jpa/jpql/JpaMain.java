f61e53fcf2a47bfbd70e3cec67c9726bf4b26f0e
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-03 11:19:52 +0900

    페이징

--- src/main/java/hello/jpa/jpql/JpaMain.java
+++ src/main/java/hello/jpa/jpql/JpaMain.java
@@ -18,35 +18,15 @@ public class JpaMain {
             member.setUsername("member1");
             member.setAge(10);
             em.persist(member);
-            
-            // 엔티티 프로젝션 -> 영속성 관리 됨
-            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class)
-                    .getResultList();
-            // 임베디드 타입 프로젝션 (엔티티로부터 시작해야 함)
-            List<Address> embedded = em.createQuery("select o.address from Order o", Address.class)
-                    .getResultList();
-            // 스칼라 타입 프로젝션 (기본 데이터 타입)
-            em.createQuery("select distinct m.username from Member m")
-                    .getResultList();
 
+            em.flush();
+            em.clear();
 
-            // 1-1 Object[] 로 여러가지 값 조회
-            List resultList1 = em.createQuery("select distinct m.username, m.age from Member m")
+            // 페이징 기능 -> DB에 맞게 쿼리를 만들어서 페이징 (추상화)
+            em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
                     .getResultList();
-
-            Object o = resultList1.get(0);
-            Object[] objArray = (Object[]) o;
-            System.out.println("username = " + objArray[0]);
-            System.out.println("age = " + objArray[1]);
-
-            // 1-2 제네릭 Object[] 로 여러가지 값 조회
-            List<Object[]> resultList2 = em.createQuery("select distinct m.username, m.age from Member m")
-                    .getResultList();
-
-            // 1-3 new 명령어로 여러가지 값 조회
-            List<MemberDTO> resultList3 = em.createQuery("select distinct new jpql.MemberDTO (m.username, m.age) from Member m", MemberDTO.class)
-                    .getResultList();
-
 
             tx.commit();
         } catch (Exception e) {
