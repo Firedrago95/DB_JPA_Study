dfd172a7dab8a89f5f7ed9ba063924f11137c38a
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-03 10:55:22 +0900

    프로젝션 - 여러 값 조회

--- src/main/java/hello/jpa/jpql/JpaMain.java
+++ src/main/java/hello/jpa/jpql/JpaMain.java
@@ -18,27 +18,35 @@ public class JpaMain {
             member.setUsername("member1");
             member.setAge(10);
             em.persist(member);
-
-            // getResultList() : 결과없으면 빈 리스트 반환 (null 걱정 x)
-            List<Member> resultList = em.createQuery("select m from Member m", Member.class)
+            
+            // 엔티티 프로젝션 -> 영속성 관리 됨
+            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class)
+                    .getResultList();
+            // 임베디드 타입 프로젝션 (엔티티로부터 시작해야 함)
+            List<Address> embedded = em.createQuery("select o.address from Order o", Address.class)
+                    .getResultList();
+            // 스칼라 타입 프로젝션 (기본 데이터 타입)
+            em.createQuery("select distinct m.username from Member m")
                     .getResultList();
 
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
 
-            // getSingleResult( ) -> 반환값 하나일때 , 결과없거나, 둘 이상이면 예외발생
-            Member singleResult = em.createQuery("select m from Member m where m.id = 1", Member.class)
-                    .getSingleResult();
+            // 1-1 Object[] 로 여러가지 값 조회
+            List resultList1 = em.createQuery("select distinct m.username, m.age from Member m")
+                    .getResultList();
 
-            // 파라미터 바인딩 이름 바인딩을 사용하자
-            em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1");
-            // 웬만하면 쓰지말자 (위치 바뀌면 오류 가능성)
-            em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter(1, "member1");
+            Object o = resultList1.get(0);
+            Object[] objArray = (Object[]) o;
+            System.out.println("username = " + objArray[0]);
+            System.out.println("age = " + objArray[1]);
 
-            //
+            // 1-2 제네릭 Object[] 로 여러가지 값 조회
+            List<Object[]> resultList2 = em.createQuery("select distinct m.username, m.age from Member m")
+                    .getResultList();
+
+            // 1-3 new 명령어로 여러가지 값 조회
+            List<MemberDTO> resultList3 = em.createQuery("select distinct new jpql.MemberDTO (m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
 
             tx.commit();
         } catch (Exception e) {
