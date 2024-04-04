21986083b57051c733ecd0109cf369a1249f2271
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-04 10:33:10 +0900

    컬렉션 값 연관경로

--- src/main/java/hello/jpa/jpql/JpaMain.java
+++ src/main/java/hello/jpa/jpql/JpaMain.java
@@ -2,7 +2,9 @@ package hello.jpa.jpql;
 
 import jakarta.persistence.*;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,25 +16,29 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team  = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select m.team From Member m";
+            // From 절에서 명시적 조인으로 별칭을 얻으면 별칭으로 탐색가능
+            String query = "select m From Team t join t.members m";
 
-            List<Team> result = em.createQuery(query, Team.class)
+            List<Collection> result = em.createQuery(query, Collection.class)
                     .getResultList();
 
-            for (Team s : result) {
-                System.out.println("s = " + s);
-            }
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e) {
