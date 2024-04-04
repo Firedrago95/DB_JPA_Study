6982a108036ec9fdf98c648f0a4efb79814eef46
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-04 09:57:49 +0900

    조건식 쿼리

--- src/main/java/hello/jpa/jpql/JpaMain.java
+++ src/main/java/hello/jpa/jpql/JpaMain.java
@@ -29,11 +29,14 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            // Enum 조회시에는 패키지명을 포함해야한다.
-            String query = "select m.username, 'HELLO', true from Member m" +
-                           "where m.type = jpql.MemberType.ADMIN";
-            List<Member> result = em.createQuery(query, Member.class)
-                    .getResultList();
+            String query =
+                    "select" +
+                            "case when m.age <= 10 then '학생요금' " +
+                            "     when m.age >= 60 then '경로요금' " +
+                            "     else '일반요금' " +
+                            "end" +
+                    "from Member m";
+            em.createQuery(query);
 
             tx.commit();
         } catch (Exception e) {
