19513b02a303a5250fde75c72705e53a12d3b96e
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-04 09:59:17 +0900

    coalesce 함수

--- src/main/java/hello/jpa/jpql/JpaMain.java
+++ src/main/java/hello/jpa/jpql/JpaMain.java
@@ -29,13 +29,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query =
-                    "select" +
-                            "case when m.age <= 10 then '학생요금' " +
-                            "     when m.age >= 60 then '경로요금' " +
-                            "     else '일반요금' " +
-                            "end" +
-                    "from Member m";
+            String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
             em.createQuery(query);
 
             tx.commit();
