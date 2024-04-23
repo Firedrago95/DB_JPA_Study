452633092096f15f3471b0233bbf8ef7b198ab05
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-23 11:33:41 +0900

    @Query DTO 와 값 조회하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -13,4 +14,16 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 //    @Query(name = "Member.findByUsername") 생략가능 메서드명으로 호출할수 있다.
     List<Member> findByUsername(@Param("username") String username);
+
+//  실무에서는 메소드 이름으로 쿼리 생성 기능은 파라미터가 증가하면 메서드 이름이 매우 지저분해진다.
+//  따라서 @Query 기능을 자주 사용하게 된다.
+    @Query("select m from Member where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " +
+            "from Member m join Team t")
+    List<MemberDto> findMemberDto();
 }
