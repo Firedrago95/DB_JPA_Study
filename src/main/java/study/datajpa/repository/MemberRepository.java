9903e6b25c3ea10034b80a9d142293c26fc0134e
YONGHWA\ekore <yonghwa95@naver.com>
2024-04-24 11:35:05 +0900

    스프링 데이터 JPA로 벌크성 수정쿼리

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
@@ -49,5 +50,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query(value = "select m from Member m left join m.team t",
             countQuery = "select count(m) from Member m")
     Page<Member> findByAge(int age, Pageable pageable);
+
+    // 벌크성 수정 쿼리는 영속성 컨텍스트와 무관하게 동작한다.
+    // 영속성 컨텍스트 비워놓거나, 수정 후 영속성 컨텍스트를 비워줘야한다.
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
 
 }
