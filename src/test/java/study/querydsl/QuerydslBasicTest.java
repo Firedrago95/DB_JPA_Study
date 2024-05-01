60ed9f874a49f1a7f6da659a0d58fcfd88f09183
YONGHWA\ekore <yonghwa95@naver.com>
2024-05-01 10:44:11 +0900

    결과 조회

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
@@ -91,5 +92,33 @@ public class QuerydslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
+    @Test
+    void resultFetch() {
+        // List 조회 -> fetch()
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
 
+        // 단건조회 -> fetchOne()
+        Member findMember1 = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        // 처음 한 건 조회
+        Member findMember2 = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        // 페이징에서 사용
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        long total = results.getTotal();
+        List<Member> contents = results.getResults();
+
+        // count 쿼리로 변경
+        long count = queryFactory.selectFrom(member)
+                .fetchCount();
+    }
 }
